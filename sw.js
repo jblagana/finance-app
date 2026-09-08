9ebf71c3eeb7442e0c9a84189794c589f96574c4
jblagana <jan.rhey.lagana@eee.upd.edu.ph>
2026-09-08 18:24:14 +0800

    v29: bot icons on a light background

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Finance PWA service worker: cache-first app shell, network-only API. */
-const CACHE = 'finances-pwa-v28';
+const CACHE = 'finances-pwa-v29';
 const SHELL = ['./', './index.html', './app.js', './chat.js', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
